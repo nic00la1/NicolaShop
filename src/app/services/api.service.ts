53e59e2e39d5a9e11e55249937c9cff5e09d4f4c
nic00la <nic00la.kaleta@gmail.com>
2024-05-19 20:43:07 +0200

    Sprawdza czy jest tyle produktu na magazynie ✅

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,17 +1,12 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {map} from "rxjs";
+import {Injectable} from '@angular/core';
+import {of} from "rxjs";
+import products from '../../assets/products.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  http = inject(HttpClient);
-
-  getProduct(){
-    return this.http.get<any>("https://fakestoreapi.com/products")
-      .pipe(map((res:any)=>{
-        return res;
-      }))
+  getProduct() {
+    return of(products);
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,6 +7,8 @@ import {BehaviorSubject} from "rxjs";
 export class CartService {
 
   public cartItemList : any =[]
+  public totalQuantity = new BehaviorSubject<number>(0);
+  public totalPrice = new BehaviorSubject<number>(0);
   public productList = new BehaviorSubject<any>([]);
   public search = new BehaviorSubject<string>("");
 
@@ -17,12 +19,28 @@ export class CartService {
   setProduct(product : any){
     this.cartItemList.push(...product);
     this.productList.next(product);
+    this.updateCartTotals();
   }
   addtoCart(product : any){
-    this.cartItemList.push(product);
+    const cartItem = this.cartItemList.find((item : any) => {
+      return item.id === product.id;
+    });
+
+    if (cartItem) {
+      if (cartItem.quantity + 1 > product.stock) {
+        alert('Nie ma tyle produktów na magazynie');
+        return;
+      }
+      cartItem.quantity += 1;
+    } else {
+      if (product.stock < 1) {
+        alert('Nie ma tyle produktów na magazynie');
+        return;
+      }
+      this.cartItemList.push({...product, quantity: 1});
+    }
     this.productList.next(this.cartItemList);
     this.getTotalPrice();
-    console.log(this.cartItemList)
   }
   getTotalPrice() : number{
     let grandTotal = 0;
@@ -31,6 +49,12 @@ export class CartService {
     })
     return grandTotal;
   }
+
+  updateCartTotals() {
+    this.totalQuantity.next(this.cartItemList.reduce((acc : any, item:any) => acc + item.quantity, 0));
+    this.totalPrice.next(this.cartItemList.reduce((acc:any, item:any) => acc + item.price * item.quantity, 0));
+  }
+
   removeCartItem(product: any){
     this.cartItemList.map((a:any, index:any)=>{
       if(product.id=== a.id){
@@ -38,9 +62,11 @@ export class CartService {
       }
     })
     this.productList.next(this.cartItemList);
+    this.updateCartTotals();
   }
   removeAllCart(){
     this.cartItemList = []
     this.productList.next(this.cartItemList);
+    this.updateCartTotals();
   }
 }
