96bf1d5114e8bc2a928a9981b90dd8679a82c494
nic00la <nic00la.kaleta@gmail.com>
2024-05-19 19:44:00 +0200

    Changed visuality 👀

--- src/app/components/pages/cart/cart.component.ts
+++ src/app/components/pages/cart/cart.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {CartService} from "../../../services/cart.service";
 import {RouterLink} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
